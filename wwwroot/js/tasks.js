6b8d478cf2ac7ec5a726f1d720cb32d2177d2701
Yavuz coşar <yavuzcosar@outlook.com>
2023-03-23 19:06:19 +0300

    add tasks

---
+++ wwwroot/js/tasks.js
@@ -0,0 +1,38 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/Tasks")
+    .withAutomaticReconnect()
+    .configureLogging(signalR.LogLevel.Debug)
+    .build();
+
+connection.on('Started', function (task) {
+    StepLog("Started", task);
+    $("#taskProgress").show();
+});
+connection.on('Progress', function (task) {
+    StepLog("Progress", task);
+    $("#taskProgress  .progress .progress-bar").css(
+        'width',
+        task.Progress + '%'
+    );
+    $("#taskProgress .d-flex .progress-percent").text(task.Progress + '%');
+    $("#taskProgress .d-flex .progress-label").text(task.Title);
+});
+connection.on('Ended', function (task) {
+    StepLog("Ended", task);
+    toastr.success("Dağıtım Tamamlandı");
+});
+ 
+connection.on("OnError", function (data) {
+    toastr.error(data, "Hata");
+});
+
+connection.start().then(function () {  
+   // TaskReady();
+});
+function StepLog(step,m) {
+    console.log(step,m);
+};
+function TaskStart(name, parameter) {
+    StepLog("Starting", name + "-->" + parameter);
+    connection.invoke("Start", name, parameter);
+}
